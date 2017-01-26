800d96fd6f89cc066c9fd35676424696df27c20a
tobyret <toby.retallick@gmail.com>
2017-01-26 23:39:59 +0000

    Wire up check-in form

--- app/checkinForm.jsx
+++ app/checkinForm.jsx
@@ -2,19 +2,42 @@ import React from 'react'
 
 export default class CheckinForm extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: '',
+      yesterday: '',
+      today: '',
+      questions: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+
+
   render () {
     return (
       <div className='pure-u-6-24'>
-        <form className="pure-form pure-form-stacked">
-          <label htmlFor="email">Date</label>
-          <input id="email" type="date" />
-          <label htmlFor="email">Yesterday</label>
-          <input id="email" type="text" />
-          <label htmlFor="email">Today</label>
-          <input id="email" type="text" />
-          <label htmlFor="email">Questions/Blockers</label>
-          <input id="email" type="text" />
-          <button type="submit" className="pure-button pure-button-primary">Check-in</button>
+        <form className='pure-form pure-form-stacked'>
+          <label>Date</label>
+          <input name='date' value={this.state.date} onChange={this.handleChange} type='date' />
+          <label>Yesterday</label>
+          <input name='yesterday' value={this.state.yesterday} onChange={this.handleChange} type='text' />
+          <label>Today</label>
+          <input name='today' value={this.state.today} onChange={this.handleChange} type='text' />
+          <label>Questions / Blockers</label>
+          <input name='questions' value={this.state.questions} onChange={this.handleChange} type='text' />
+          <button onClick={this.handleSubmit} className='pure-button pure-button-primary'>Check-in</button>
         </form>
       </div>
     )
