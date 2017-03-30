447c797c6b70e04a8126f99fd48207dbf8e8c675
tobyret <toby.retallick@gmail.com>
2017-03-30 13:39:31 +0100

    Wire up mongodb as a docker container

--- server.js
+++ server.js
@@ -3,7 +3,36 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 8080
 
+const MongoClient = require('mongodb').MongoClient
+let db
+
+MongoClient.connect('mongodb://localhost:3000', (err, database) => {
+  if(err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('Mongo listening on 3000')
+  })
+})
+
 app.use('/', express.static('build'))
+
+app.get('/save-hello', (req, res) => {
+  db.collection('greeting')
+    .insertOne({
+      greeting: 'Bonjour!'
+    },
+      () => { console.log('Mongo successful')})
+  res.send('Hello saved')
+})
+
+app.get('/get-hello', (req, res) => {
+  const greeting = db.collection('greeting').find().toArray().then(
+    greeting => {
+      console.log("GREETING!", greeting)
+      res.send(greeting)
+    }
+  )
+})
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'))
